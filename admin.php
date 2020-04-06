ce297e246f5433ea2c11afbca3015769a5288a04
Sergio Lo Porto <sergioloporto@gmail.com>
2020-04-06 18:00:35 +0200

    added background color select, and bar text selection options

--- admin.php
+++ admin.php
@@ -1,17 +1,78 @@
 <?php
-add_action( 'admin_menu', 'wprcb_menu' );
+add_action('admin_menu', 'wprcb_menu');
+add_action('admin_init', 'wprcb_api_settings_init');
 
-function wprcb_menu() {
-	add_options_page( 'My Plugin Options', 'WP Reliable Cookie Bar', 'manage_options', 'wprcb-settings', 'wprcb_options' );
+function wprcb_menu()
+{
+	add_options_page('My Plugin Options', 'WP Reliable Cookie Bar', 'manage_options', 'wprcb-settings', 'wprcb_options');
 }
+
+
+function wprcb_api_settings_init()
+{
+	register_setting('wprcbPlugin', 'wprcb_api_settings');
+	add_settings_section(
+		'wprcb_api_wprcbPlugin_section',
+		__('General settings', 'wprbc'),
+		'wprcb_api_settings_section_callback',
+		'wprcbPlugin'
+	);
+
+	add_settings_field(
+		'wprcb_api_text_field_cookieMessage',
+		__('Cookie Bar Text', 'wprbc'),
+		'wprcb_api_text_field_cookieMessage_render',
+		'wprcbPlugin',
+		'wprcb_api_wprcbPlugin_section'
+	);
+
+	add_settings_field(
+		'wprcb_api_color_field_backgroundColor',
+		__('Background color', 'wprbc'),
+		'wprcb_api_color_field_backgroundColor_render',
+		'wprcbPlugin',
+		'wprcb_api_wprcbPlugin_section'
+	);
+}
+
+
+function wprcb_api_text_field_cookieMessage_render()
+{
+	$options = get_option('wprcb_api_settings');
+?>
+	<input type='text' name='wprcb_api_settings[wprcb_api_text_field_cookieMessage]' value='<?php echo $options['wprcb_api_text_field_cookieMessage']; ?>' placeholder="This site uses internal and external cookies provide and improve our services. By using our site, you grant consent to cookies." >
+<?php
+}
+
+function wprcb_api_color_field_backgroundColor_render(  ) {
+    $options = get_option( 'wprcb_api_settings' );
+	?>
+	<input type="color" id="favcolor" name="wprcb_api_settings[wprcb_api_color_field_backgroundColor]" value="#000" > 
+<?php
+}
+
+function wprcb_api_settings_section_callback()
+{
+	echo __('In this page you can change text and the visual aspect of the WP Reliable Cookie Bar', 'wprcb');
+}
+
 
 /** Step 3. */
-function wprcb_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+function wprcb_options()
+{
+	if (!current_user_can('manage_options')) {
+		wp_die(__('You do not have sufficient permissions to access this page.'));
 	}
-	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
-	echo '</div>';
-}
 ?>
+	<form action='options.php' method='post'>
+		<h2>Sitepoint Settings API Admin Page</h2>
+
+		<?php
+		settings_fields('wprcbPlugin');
+		do_settings_sections('wprcbPlugin');
+		submit_button();
+		?>
+
+	</form>
+<?php
+}
--- wp-reliable-cookie-bar.php
+++ wp-reliable-cookie-bar.php
@@ -23,12 +23,17 @@ add_action('get_footer', 'wprcb_add_footer_styles');
 
 function wprcb_inject_html_into_footer()
 {
+    $options = get_option( 'wprcb_api_settings' );
+    $text_to_display = $options['wprcb_api_text_field_cookieMessage'] ? $options['wprcb_api_text_field_cookieMessage'] : 'This site uses internal and external cookies provide and improve our services. By using our site, you grant consent to cookies.';
+
+    $background_color = $options['wprcb_api_select_field_backgroundColor'] ? $options['wprcb_api_select_field_backgroundColor'] : '#000';
+
     //echo '<p><script async="" defer="" src="//widget.getyourguide.com/v2/widget.js"></script></p>';
     echo <<<COOKIEBARHTML
-  <div id="wprcb-cookie-bar" class="wprcb-cookie-bar" style="display: flex;">
+  <div id="wprcb-cookie-bar" class="wprcb-cookie-bar" style="display: flex; background:{$background_color}">
   <div>
     <div class="wprcb-content">
-      <p class="wprcb-text">This site uses internal and external cookies provide and improve our services. By using our site, you grant consent to cookies.</p>
+      <p class="wprcb-text">{$text_to_display}</p>
       <a class="accept-button" id="accept-button" href="#">Accept</a>
       <a class="learn-more" href="{$GLOBALS['wprcb_policy']}" target="_blank" rel="nofollow">Learn more</a>
       </div>
