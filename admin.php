ed710925e1d0401eec9985efc66112d7310b64df
Sergio Lo Porto <sergioloporto@gmail.com>
2020-04-09 00:48:14 +0200

    added new sections

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@ function wprcb_menu()
 
 function wprcb_api_settings_init()
 {
-	register_setting('wprcbPlugin', 'wprcb_api_settings', 'wprcb_validate_options');
+	register_setting('wprcbPlugin', 'wprcb_api_settings');
 	add_settings_section(
 		'wprcb_api_wprcbPlugin_section',
 		__('General settings', 'wprbc'),
@@ -35,6 +35,22 @@ function wprcb_api_settings_init()
 	);
 
 	add_settings_field(
+		'wprcb_api_color_field_textColor',
+		__('Text color', 'wprbc'),
+		'wprcb_api_color_field_textColor_render',
+		'wprcbPlugin',
+		'wprcb_api_wprcbPlugin_section'
+	);
+
+	add_settings_field(
+		'wprcb_api_boolean_showHide_border',
+		__('Show/hide border', 'wprbc'),
+		'wprcb_api_boolean_showHide_border_render',
+		'wprcbPlugin',
+		'wprcb_api_wprcbPlugin_section'
+	);
+
+	add_settings_field(
 		'wprcb_api_number_field_barHeight',
 		__('Cookie bar height', 'wprbc'),
 		'wprcb_api_number_field_barHeight_render',
@@ -45,21 +61,33 @@ function wprcb_api_settings_init()
 
 
 	// Button section
-	register_setting('wprcbPlugin_Button', 'wprcb_api_settings_Button');
+	// register_setting('wprcbPlugin_Button', 'wprcb_api_settings_Button');
 	add_settings_section(
 		'wprcb_api_wprcbPlugin_section_button',
 		__('Button properties', 'wprbc'),
 		'wprcb_api_button_settings_section_callback',
-		'wprcbPlugin_Button'
+		'wprcbPlugin'
 	);
 
 	add_settings_field(
 		'wprcb_api_color_field_button_backgroundColor',
 		__('Button background color', 'wprbc'),
 		'wprcb_api_color_field_button_backgroundColor_render',
-		'wprcbPlugin_Button',
+		'wprcbPlugin',
 		'wprcb_api_wprcbPlugin_section_button'
 	);
+	add_settings_field(
+		'wprcb_api_color_field_button_textColor',
+		__('Button text color', 'wprbc'),
+		'wprcb_api_color_field_button_textColor_render',
+		'wprcbPlugin',
+		'wprcb_api_wprcbPlugin_section_button'
+	);
+}
+
+function wprcb_api_settings_section_callback()
+{
+	echo __('In this page you can change text and the visual aspect of the WP Reliable Cookie Bar', 'wprcb');
 }
 
 function wprcb_api_text_field_cookieMessage_render()
@@ -72,10 +100,20 @@ function wprcb_api_text_field_cookieMessage_render()
 
 function wprcb_api_color_field_backgroundColor_render()
 {
+	$options = get_option('wprcb_api_settings');
 ?>
-	<input type="color" id="favcolor" name="wprcb_api_settings[wprcb_api_color_field_backgroundColor]" value="#000">
+	<input type="color" id="favcolor" name="wprcb_api_settings[wprcb_api_color_field_backgroundColor]" value="<?php echo $options['wprcb_api_color_field_backgroundColor']; ?>">
 <?php
 }
+
+function wprcb_api_color_field_textColor_render()
+{
+	$button_options = get_option( 'wprcb_api_settings' );
+?>
+	<input type="color" id="favcolor" name="wprcb_api_settings[wprcb_api_color_field_textColor]" value="<?php echo $button_options['wprcb_api_color_field_textColor']; ?>">
+<?php
+}
+
 
 function wprcb_api_number_field_barHeight_render($args)
 {
@@ -86,41 +124,42 @@ function wprcb_api_number_field_barHeight_render($args)
 <?php
 }
 
-function wprcb_api_color_field_button_backgroundColor_render()
+function wprcb_api_boolean_showHide_border_render()
 {
-	$button_options = get_option( 'wprcb_api_settings_Button' );
+	$options = get_option('wprcb_api_settings');
 ?>
-	<input type="color" id="favcolor" name="wprcb_api_settings_Button[wprcb_api_color_field_button_backgroundColor]" value="<?php echo $button_options['wprcb_api_color_field_button_backgroundColor']; ?>">
+	<select id="show-hide-border" name="wprcb_api_settings[wprcb_api_boolean_showHide_border]">	
+		<option value="1" <?php selected( $options['wprcb_api_boolean_showHide_border'], 1 ); ?>>Show</option>
+		<option value="0" <?php selected( $options['wprcb_api_boolean_showHide_border'], 0 ); ?>>Hide</option>
+	</select>
 <?php
 }
 
-function wprcb_api_settings_section_callback()
-{
-	echo __('In this page you can change text and the visual aspect of the WP Reliable Cookie Bar', 'wprcb');
-}
 function wprcb_api_button_settings_section_callback()
 {
 	echo __('Button options', 'wprcb');
 }
 
-$text_string = $options['wprcb_api_text_field_cookieMessage'];
-function wprcb_validate_options( $input ) {
-    $valid['wprcb_api_text_field_cookieMessage'] = preg_replace( '/[^a-zA-Z]/', '', $input['wprcb_api_text_field_cookieMessage'] );
 
-    if( $valid['wprcb_api_text_field_cookieMessage'] != $input['wprcb_api_text_field_cookieMessage'] ) {
-        add_settings_error(
-            'wprcb_api_text_field_cookieMessage',
-            'boj_myplugin_texterror',
-            'Incorrect value entered!',
-            'error'
-        );      
-    }
+function wprcb_api_color_field_button_backgroundColor_render()
+{
+	$button_options = get_option( 'wprcb_api_settings' );
+?>
+	<input type="color" id="favcolor" name="wprcb_api_settings[wprcb_api_color_field_button_backgroundColor]" value="<?php echo $button_options['wprcb_api_color_field_button_backgroundColor']; ?>">
+<?php
+}
 
-    return $valid;
+function wprcb_api_color_field_button_textColor_render()
+{
+	$button_options = get_option( 'wprcb_api_settings' );
+?>
+	<input type="color" id="favcolor" name="wprcb_api_settings[wprcb_api_color_field_button_textColor]" value="<?php echo $button_options['wprcb_api_color_field_button_textColor']; ?>">
+<?php
 }
 
 
 
+/** Step 3. */
 function wprcb_options()
 {
 	if (!current_user_can('manage_options')) {
@@ -133,8 +172,6 @@ function wprcb_options()
 		<?php
 		settings_fields('wprcbPlugin');
 		do_settings_sections('wprcbPlugin');
-		do_settings_sections('wprcbPlugin_Button');
-		settings_fields('wprcbPlugin_Button');
 
 		submit_button();
 		?>
